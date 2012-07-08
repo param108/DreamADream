7ed43328f9de894caed0e3d151d7cf08f4a0203c
Paramananda <pponnaiyan@ubuntu.(none)>
2012-07-08 09:31:42 -0700

    Fixed captcha for website.

--- trylogin.php
+++ trylogin.php
@@ -12,7 +12,7 @@ if (array_key_exists("username",$_POST)) {
 	  exit(1);
 	}
 	
-	if ($loginDetails['retries'] > 3 && !array_key_exists("captchaText",$_POST)) {
+	if ($loginDetails['retries'] > 3 && !array_key_exists("captcha_code",$_POST)) {
 	  header("Location: index.php?retries=".$loginDetails['retries']);
 	  exit(1);
 	}
@@ -20,16 +20,27 @@ if (array_key_exists("username",$_POST)) {
 	if ($loginDetails['password'] == sha1($_POST['password'])) {
 	  if ($loginDetails['retries'] > 3) {
 	    //check captcha
+	    $retries = $loginDetails['retries'];
 	    $secureimage=new Securimage();
-	    
+	    if ($secureimage->check($_POST['captcha_code']) == true) {
+	      DBHandler::resetLoginRetries($_POST['username']);
+	      // fall through to the end for success
+	    } else {
+	      header("Location: index.php?retries=$retries");
+	      exit(1);
+	    }  
+	  } else {
+	    DBHandler::resetLoginRetries($_POST['username']);
 	  }
-	  DBHandler::resetLoginRetries($_POST['username']);
-	  die("Login Successful");
+	  // fall through to end for successful login
+	} else {
+	  DBHandler::incLoginRetries($_POST['username']);
+	  $retries = $loginDetails['retries'] + 1;
+	  header("Location: index.php?retries=$retries");
+	  exit(1);
 	}
-	DBHandler::incLoginRetries($_POST['username']);
-	$retries = $loginDetails['retries'] + 1;
-	header("Location: index.php?retries=$retries");
-	exit(1);
+	// success reach here.
+	die("Login Successful");
 } else {
   // redirect back to index.php
   header( 'Location: index.php');
